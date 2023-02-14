4748395c68c91950e5ed6c170c42a3ac26cc46c0
Sathish <Sekaran.SathishKumar@in.bosch.com>
2023-02-15 00:32:49 +0530

    Delete multiple definition

--- ReadSensorVal.c
+++ ReadSensorVal.c
@@ -3,6 +3,12 @@
 #include <string.h>
 #include "ReadSensorVal.h"
 
+char Sensorname[Max_Sensor][100] = 
+{
+    "Temparature Sensor",
+    "Current Sensor",
+};
+
 void Get_Sensor_Readings(float maxRange,int maxReadings,float* SensorOut)
 {
     for(int loopCnt=0;loopCnt<maxReadings;loopCnt++)
--- ReadSensorVal.h
+++ ReadSensorVal.h
@@ -5,14 +5,8 @@ enum
     Max_Sensor,
 }en_Sensor;
 
-char Sensorname[Max_Sensor][100] = 
-{
-    "Temparature Sensor",
-    "Current Sensor",
-};
-
-
 #define MAX_TEMP_RANGE          50
 #define MAX_Current_RANGE       10
 
+extern char Sensorname[Max_Sensor][100];
 void Get_Sensor_Readings(float maxRange,int maxReadings,float* SensorOut);
