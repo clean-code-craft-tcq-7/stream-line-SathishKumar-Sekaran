159bc1698599918c301077a41446c2cfba151cd3
Sathish <Sekaran.SathishKumar@in.bosch.com>
2023-02-15 00:09:13 +0530

    Error clear

--- Sender.h
+++ Sender.h
@@ -1,2 +1,6 @@
+typedef void (*FuncPtrTestSenderType) (void);
 
+typedef int (*Func2PtrSenderType)(char*);
+
+void SendToServer(void);
 void GetTx_Sensor_val(float *SensorData,int totalSensorCnt,int totalSensorReadCnt,char *tx_Buffer)
