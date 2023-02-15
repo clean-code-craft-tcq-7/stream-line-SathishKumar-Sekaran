834196deab693f68137292209612210bee025999
Sathish <Sekaran.SathishKumar@in.bosch.com>
2023-02-15 12:46:52 +0530

    Function argument changed

--- Sender.c
+++ Sender.c
@@ -50,9 +50,10 @@ void GetTx_Sensor_val(float *SensorData,int totalSensorCnt,int totalSensorReadCn
     }
 }
 
-void SendToServer(char *buf)
+void SendToServer()
 {
     float SensorOut[Max_Sensor][50];
+    char buf[500];
     memset(buf,'\0',500);
     Get_Sensor_Readings(MAX_TEMP_RANGE,50,SensorOut[Temparature]);
     Get_Sensor_Readings(MAX_TEMP_RANGE,50,SensorOut[Current]);
